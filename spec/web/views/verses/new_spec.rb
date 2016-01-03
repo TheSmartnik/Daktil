1ac79e119ddfc4cef006b59b017a289502cd2129
Nikita Misharin <misharinn@gmail.com>
2016-01-04 01:09:28 +0300

    Cleanup

--- spec/web/views/verses/new_spec.rb
+++ spec/web/views/verses/new_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../../apps/web/views/verses/new'
 
 describe Web::Views::Verses::New do
   let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Lotus::View::Template.new('apps/web/templates/verses/new.html.erb') }
+  let(:template)  { Lotus::View::Template.new('apps/web/templates/verses/new.html.slim') }
   let(:view)      { Web::Views::Verses::New.new(template, exposures) }
   let(:rendered)  { view.render }
 
