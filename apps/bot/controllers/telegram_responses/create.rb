864bedb3aa65c8f1254d6a9d2ed6aaafb65e0ac9
Nikita Misharin <misharinn@gmail.com>
2016-01-14 23:52:43 +0300

    refactor

--- apps/bot/controllers/telegram_responses/create.rb
+++ apps/bot/controllers/telegram_responses/create.rb
@@ -5,12 +5,21 @@ module Bot::Controllers::TelegramResponses
     def call(params)
       chat_params = Hash params['message']['chat'].merge('text' => params['message']['text'])
       chat_params['chat_id'] = chat_params.delete('id')
-      return FailCommand.new(chat_params) unless valid?
-      "#{command.capitalize}Command".constantize.new(chat_params).execute
+
+      execute_command
+
       status 200, "All good"
     end
 
     private
+
+    def execute_command
+      if valid?
+        "#{command.capitalize}Command".constantize.new(chat_params).execute
+      else
+        FailCommand.new(chat_params)
+      end
+    end
 
     def valid?
       text = params['message']['text']
