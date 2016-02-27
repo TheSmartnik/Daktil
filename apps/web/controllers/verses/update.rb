670c91cfb618ce4dd53763230c79a4ab04851e07
Nikita Misharin <misharinn@gmail.com>
2016-02-27 05:34:05 +0300

    Fix verse creation

--- apps/web/controllers/verses/update.rb
+++ apps/web/controllers/verses/update.rb
@@ -3,9 +3,15 @@ module Web::Controllers::Verses
     include Web::Action
 
     def call(params)
-      @verse = VerseRepository.persist Verse.new(params[:verse])
+      @verse = VerseRepository.persist Verse.new(verse_params)
       redirect_to "/verses/edit/#{@verse.id}"
     end
 
+    private
+
+    def verse_params
+      attrs = Hash params[:verse]
+      attrs['id'].blank? ? attrs.merge('id' => nil) : attrs
+    end
   end
 end
